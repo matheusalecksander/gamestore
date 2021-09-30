ebd5a3113c45ce3cc91e394ac76098655b18312e
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2021-09-30 09:42:46 -0300

    correçao no tamanho e posiçao do header

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -3,10 +3,11 @@ import themes from '../../global/theme'
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 25,
+    marginTop: 40,
     paddingVertical: 15,
     paddingHorizontal: 20,
     width: '100%',
+    height: 60,
     alignItems: 'center',
     justifyContent: 'space-between',
     flexDirection: 'row',
