1bb6d6ad152666c7e17119112c876bb0b80fc4a3
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2021-10-04 19:14:11 -0300

    Terminado customização da tab bar

--- src/routes/tabsRoutes.js
+++ src/routes/tabsRoutes.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons';
+
+import { View, StyleSheet, Text } from 'react-native'
 
 import StackRoutes from './stackRoutes'
 import Cart from '../pages/Cart'
@@ -9,13 +12,30 @@ const Tab = createBottomTabNavigator()
 
 function Tabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          backgroundColor: themes.colors.background,
+      },
+    }}
+    >
       <Tab.Screen
        name="Home"
        component={StackRoutes}
        options={
         {
-          headerShown: false
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <View style={focused ? styles.iconContainerFocused : styles.iconContainer}>
+              <Ionicons
+               name="home-outline"
+               size={24}
+               color={focused ? themes.colors.secondary : themes.colors.primary}
+                />
+                <Text style={focused ? styles.labelTextFocused : styles.labelText}>HOME</Text>
+            </View>
+          )
         }
       }/>
       <Tab.Screen 
@@ -28,10 +48,52 @@ function Tabs() {
                 fontWeight: 'bold',
               },
               headerTitleAlign: 'center',
+              tabBarIcon: ({focused}) => (
+                <View style={focused ? styles.iconContainerFocused : styles.iconContainer}>
+                <Ionicons
+                  name="cart-outline"
+                  size={24}
+                  color={focused ? themes.colors.secondary : themes.colors.primary}
+                />
+                  <Text style={focused ? styles.labelTextFocused : styles.labelText}>CARRINHO</Text>
+              </View>
+          )
             }
         }/>
     </Tab.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    width: 80,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 5,
+    borderRadius: 10
+  },
+  iconContainerFocused: {
+    width: 80,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 5,
+    borderTopWidth: 3,
+    borderTopColor: themes.colors.secondary,
+    borderRadius: 10
+  },
+  labelText: {
+    fontFamily: themes.fonts.textFont,
+    fontSize: 12,
+    color: themes.colors.primary
+  },
+  labelTextFocused: {
+    fontFamily: themes.fonts.textFont,
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: themes.colors.secondary
+  }
+})
 
 export default Tabs;
